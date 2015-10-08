e01dbe4dcd0d18e92f66d0e04339343dee9f3e33
Tickeldi <ole@olegoes.de>
2015-10-08 09:00:37 +0200

    create hashcode and equals to be able use options in hashmap

--- SplusReader/src/de/ole/splusreader/logic/Option.java
+++ SplusReader/src/de/ole/splusreader/logic/Option.java
@@ -27,4 +27,35 @@ public final class Option {
 	public String toString() {
 		return number + " " + title + " (" + value + ")";
 	}
+
+	@Override
+	public int hashCode() {
+		final int prime = 31;
+		int result = 1;
+		result = prime * result + ((title == null) ? 0 : title.hashCode());
+		result = prime * result + ((value == null) ? 0 : value.hashCode());
+		return result;
+	}
+
+	@Override
+	public boolean equals(Object obj) {
+		if (this == obj)
+			return true;
+		if (obj == null)
+			return false;
+		if (getClass() != obj.getClass())
+			return false;
+		Option other = (Option) obj;
+		if (title == null) {
+			if (other.title != null)
+				return false;
+		} else if (!title.equals(other.title))
+			return false;
+		if (value == null) {
+			if (other.value != null)
+				return false;
+		} else if (!value.equals(other.value))
+			return false;
+		return true;
+	}
 }
